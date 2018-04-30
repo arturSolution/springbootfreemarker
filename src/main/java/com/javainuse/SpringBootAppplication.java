fc2c693480ebc2b645c90116e74fc7285dcb9195
Artur <ttuca123@gmail.com>
2018-04-30 07:57:53 -0300

    primeiro commit

---
+++ src/main/java/com/javainuse/SpringBootAppplication.java
@@ -0,0 +1,15 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootAppplication {
+
+	
+	public static void main(String args[]){
+		
+		SpringApplication.run(SpringBootAppplication.class, args);
+	}
+}
